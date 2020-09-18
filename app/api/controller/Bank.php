c1ad3df2c6c42315a67cb954905c141b0c2169b6
maifeigit <maifei@protonmail.com>
2020-09-18 12:29:04 +0800

    添加API接口

---
+++ app/api/controller/Bank.php
@@ -0,0 +1,119 @@
+<?php
+namespace app\api\controller;
+
+use Config;
+use think\Db;
+use Session;
+use Cache;
+use Log; 
+
+/**
+ * 收藏管理
+ */
+class Bank extends Base
+{
+
+    protected function initialize()
+    {
+        parent::initialize();
+    }
+
+
+    public function index()
+    {
+
+    }
+
+
+    // 交易请求
+    public function apply()
+    {
+        if ($this->request->isPost()) {
+            $post = $this->request->param();
+            // 平台订单号
+            $platform_sn = date('YmdHis').mt_rand(10000,99999);
+            // 订单数据
+            $data = [
+                'uid'      => $post['uid'],
+                'username' => $post['username'],
+                'amount'   => $post['amount'],
+                'sn'       => $post['sn'],
+                'app_id'   => $post['app_id'],
+                'platform_sn' => $platform_sn,
+                'create_time' =>time(),
+                'status'   => 0,
+            ];
+
+            // 保存订单数据
+            if(Db::name('deposit')->insertGetId($data)){
+                // 获取一张可用银行卡
+                $bank = Db::name('bank_card')->where(['status'=>1])->find();
+                if($bank['id']){
+                    $result = [
+                        'create_time' => time(),
+                        'uid'         => $post['uid'],
+                        'username'    => $post['username'],
+                        'sn'          => $post['sn'],
+                        'amount'      => $post['amount'],
+                        'bank_id'     => $bank['id'],
+                        'bank_name'   => $bank['bank_name'],
+                        'bank_card'   => $bank['bank_card'],
+                        'bank_person' => $bank['name'],
+                        'platform_sn' => $platform_sn,
+                        'app_id'      => $post['app_id'],
+                    ];
+                    echo json_encode($result); exit;
+                }else{
+                    $result = [
+                        'status' => 404,
+                        'msg'    => '暂无可用银行卡',
+                        'data'   => ''
+                    ;
+                    echo json_encode($result); exit;
+                }
+            } else {
+                $result = [
+                    'status' => 404,
+                    'msg'    => '订单数据保存失败',
+                    'data'   => ''
+                ;
+                echo json_encode($result); exit;
+            }
+        }
+    }
+
+    public function pic()
+    {
+        if ($this->request->isPost()) {
+            $post = $this->request->param();
+
+            $pic         = $post['pic'];            // 转账截图
+            $platform_sn = $post['platform_sn'];    // 平台单号
+
+            if(Db::name('deposit')->where(['platform_sn'=>$platform_sn])->find()){
+                // 保存转账截图
+                if (Db::name('deposit')->where(['platform_sn'=>$platform_sn])->update(['pic'=>$pic])) {
+                    $result = [
+                        'status' => 200,
+                        'msg'    => '已更新图片链接',
+                        'data'   => ''
+                    ;
+                    echo json_encode($result); exit;
+                } else {
+                    $result = [
+                        'status' => 404,
+                        'msg'    => '图片链接更新失败',
+                        'data'   => ''
+                    ;
+                    echo json_encode($result); exit;
+                }
+            }else{
+
+            }
+
+
+        }
+    }
+
+
+}
